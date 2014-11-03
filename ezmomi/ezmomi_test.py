5f68333952e23da6adfac34e2c04e3bd95cf7ce5
Maxim Kovgan <maxk@fortscale.com>
2014-11-03 20:13:20 +0200

    support for well tempered template

---
+++ ezmomi/ezmomi_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+__author__ = ','.join((
+    'Maxim Kovgan <maxk@fortscale.com>',
+))
+# File Name: ezmomi_test
+#    Created on: 02/11/14 at 12:49
+#    Copyright by Fortscale Security ltd. 2014
+
+def test_
